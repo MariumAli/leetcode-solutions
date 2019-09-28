ea1dbea2e0bccec1cd7c4a19b7c73ec2b4701199
Marium Ali <42535051+MariumAli@users.noreply.github.com>
2019-09-28 15:58:33 +0500

    Create n-repeated-element-in-size-2n-array.py
    In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
    
    Return the element repeated N times.

---
+++ n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,8 @@
+    def repeatedNTimes(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for x in A:
+            if A.count(x) == len(A)/2:
+                return x
