18d073dea8a8169632a2bc3ef65a694f1f16c25d
Marium Ali <42535051+MariumAli@users.noreply.github.com>
2019-10-01 17:47:19 +0500

    Create partition-labels.py
    A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
    
    Example 1:
    Input: S = "ababcbacadefegdehijhklij"
    Output: [9,7,8]
    Explanation:
    The partition is "ababcbaca", "defegde", "hijhklij".
    This is a partition so that each letter appears in at most one part.
    A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
    Note:
    
    S will have length in range [1, 500].
    S will consist of lowercase letters ('a' to 'z') only.

---
+++ partition-labels.py
@@ -0,0 +1,18 @@
+    def partitionLabels(self, S):
+        """
+        :type S: str
+        :rtype: List[int]"""
+        
+        rightmost = {c:i for i, c in enumerate(S)}
+        left, right = 0, 0
+
+        result = []
+        for i, letter in enumerate(S):
+
+            right = max(right,rightmost[letter])
+
+            if i == right:
+                result += [right-left + 1]
+                left = i+1
+
+        return result
